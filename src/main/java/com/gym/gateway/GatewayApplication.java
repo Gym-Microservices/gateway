3a4d5798d052e9a39ea0e7cbe5110bbef40d7c22
jdColonia <JuanDavidColoniaAldana@gmail.com>
2025-09-14 18:55:27 -0500

    feat: add security configuration and update microservices routes on the gateway

--- src/main/java/com/gym/gateway/GatewayApplication.java
+++ src/main/java/com/gym/gateway/GatewayApplication.java
@@ -3,13 +3,13 @@ package com.gym.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableWebFluxSecurity
 public class GatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
 }
